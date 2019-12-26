68eafe241519faaa6a21d42b21fb1ac8343f478b
Carlos Herrera Lagranja <cherrela@everis.com>
2019-12-26 14:32:35 +0100

    Anecdotes1d-branch - step 1, 2 & 3.

--- src/index.js
+++ src/index.js
@@ -1,27 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import Content from './components/Content/Content';
-import Total from './components/Total/Total';
-import Header from './components/Header/Header';
 
-const App = () => {
-  const content = { 
-    course: 'Half Stack application development',
-    part1: 'Fundamentals of React',
-    exercises1: 10,
-    part2: 'Using props to pass data',
-    exercises2:  7,
-    part3: 'State of a component',
-    exercises3: 14,
+const App = (props) => {
+  const [selected, setSelected] = useState(0)
+  const [points, setVoted] = useState({0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0});
+  const [max, setMax] = useState(0)
+  const setToVoted = (vote) => {
+    const copy = { ...points }
+    copy[vote] += 1 
+    setVoted(copy);
+    mostValuable(copy);
   };
+  const mostValuable = (obj) => Object.keys(obj).reduce((acc, o) => { 
+    if( acc < obj[o]) {
+      setMax(o);
+      acc = obj[o];
+    }
+    return acc;
+  }, 0);
   return (
     <div>
-      <Header header={content}/>
-      <Content content={content}/>
-      <Total total={content}/>
+      <h2>Anecdote of the day</h2>
+      <div>
+        {props.anecdotes[selected]}
+        <div>{`has `+points[selected]+` votes`}</div>
+        <div>
+          <button onClick={() => setToVoted(selected)}>
+            Vote
+          </button>
+          <button onClick={() => setSelected(Math.round(Math.random(1) * 5))}>
+            Next anecdote
+          </button>
+        </div>
+      </div>
+      <div>
+        <h2>Anecdote with most votes</h2>
+        {props.anecdotes[max]}
+        <div>{`has `+points[max]+` votes`}</div>
+      </div>
     </div>
   )
 }
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const anecdotes = [
+  'If it hurts, do it more often',
+  'Adding manpower to a late software project makes it later!',
+  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+  'Premature optimization is the root of all evil.',
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+]
+
+ReactDOM.render(
+  <App anecdotes={anecdotes} />,
+  document.getElementById('root')
+)
